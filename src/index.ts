5c57149ae07ca72afcfd23fb16497e25372f55b6
Pedro Guedes <pedro.guedes@dreamlines.com>
2022-11-03 11:06:05 -0300

    main: improve variable names and add progress bar

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,49 @@
 import * as moment from "moment";
-import { getRecursosPorFavorecidos, getRelacaoIndicacoes2022 } from "./sources";
+import * as csv from "csvtojson";
+import * as path from "path";
+import { getRecursosPorFavorecidos, getDocumentoComissaoPlanos2022 } from "./sources";
+const cliProgress = require("cli-progress");
 
 async function main() {
   const municipios = {};
 
-  const recursosPorFavorecidoCache = await getRecursosPorFavorecidos();
-  const relacaoIndicacoes2022 = await getRelacaoIndicacoes2022();
+  const recursosPorFavorecidoPorAnoMes = await getRecursosPorFavorecidos();
+  const documentoComissao2022 = await getDocumentoComissaoPlanos2022();
+  const rawDictionaryFromCsv = await csv({ headers: ["beneficiario", "favorecido"], delimiter: ";" }).fromFile(
+    path.join(__dirname, "../dicionario.csv")
+  );
+  const dictionaryDocumentoComissaoParaPortalTransparencia = Object.fromEntries(
+    rawDictionaryFromCsv.map((line) => [line["beneficiario"], line["favorecido"]])
+  );
 
-  console.log("total", relacaoIndicacoes2022.length);
-  for (const row of relacaoIndicacoes2022) {
-    const anoMes = moment(row["Data de cadastro"]).format("YYYYMM");
+  console.log("Buscando Recursos por Favorecidos (Portal da Transparência) no cache");
+  const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+  bar1.start(documentoComissao2022.length, 0);
+  const errors = [];
+  let i = 0;
+  for (const linhaDocumentoComissao of documentoComissao2022) {
+    try {
+      const anoMes = moment(linhaDocumentoComissao["Data de cadastro"]).format("YYYYMM");
 
-    const resultadoDb = recursosPorFavorecidoCache[anoMes];
+      const recursosPorFavorecido = recursosPorFavorecidoPorAnoMes[anoMes];
 
-    const registroEquivalente = resultadoDb.find((row) => row["Nome Favorecido"] === row["Nome beneficiário"]);
+      const registroEquivalente = recursosPorFavorecido.find(
+        (row) => row["Nome Favorecido"] === dictionaryDocumentoComissaoParaPortalTransparencia[row["Nome Favorecido"]]
+      );
 
-    const valor = Number(registroEquivalente["Valor Recebido"].replace(",", "."));
+      const valor = Number(registroEquivalente["Valor Recebido"].replace(",", "."));
 
-    if (!municipios[row["Nome Município"]]) {
-      municipios[row["Nome Município"]] = valor;
-    } else {
-      municipios[row["Nome Município"]] += valor;
+      if (!municipios[linhaDocumentoComissao["Nome Município"]]) {
+        municipios[linhaDocumentoComissao["Nome Município"]] = valor;
+      } else {
+        municipios[linhaDocumentoComissao["Nome Município"]] += valor;
+      }
+      bar1.update(i++);
+    } catch (error) {
+      errors.push(error);
     }
-
-    console.count("item");
   }
+  bar1.stop();
   console.log(Object.fromEntries(Object.entries<number>(municipios).sort(([, a], [, b]) => a - b)));
 }
 
